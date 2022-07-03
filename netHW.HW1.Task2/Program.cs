db36ab9ee4fd77bbd8dbce3063c3c5b87af9a94d
alena.dorofeeva <zelenaya_lena@mail.ru>
2022-07-03 16:30:24 +0300

    netHW.HW1.Task2

---
+++ netHW.HW1.Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace netHW.HW1.Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's learn the multiplication table.");
+            Console.WriteLine("Enter integer number please:");
+            
+            bool isCorrectValue = int.TryParse(Console.ReadLine(), out int UserInputValue);
+            if (isCorrectValue)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{UserInputValue} * {i} = {Multiply(UserInputValue, i)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid value entered");
+            }
+
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+}
